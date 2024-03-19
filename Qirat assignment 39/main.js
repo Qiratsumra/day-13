c24e6e8ad9447280a9f27f2d8424280998932adc
[Qiratsumra] <[qiratsaeed5@gmail.com]>
2024-03-19 13:53:34 +0500

    first commit

---
+++ Qirat assignment 39/main.js
@@ -0,0 +1,11 @@
+function cityCountry(city, country) {
+    return "".concat(city, ",").concat(country);
+}
+var c1 = cityCountry("Karachi", "Pakistan");
+var c2 = cityCountry("Lahore", "Pakistan");
+var c3 = cityCountry("Istanbul", "Turkey");
+var c4 = cityCountry("Paris", "France");
+console.log(c1);
+console.log(c2);
+console.log(c3);
+console.log(c4);
---
+++ Qirat assignment 39/main.ts
@@ -0,0 +1,14 @@
+function cityCountry(city:string, country:string) :string{
+    return`${city},${country}`
+}
+
+let c1=cityCountry(`Karachi`,`Pakistan`);
+let c2=cityCountry(`Lahore`,`Pakistan`);
+let c3=cityCountry(`Istanbul`,`Turkey`);
+let c4=cityCountry(`Paris`,`France`);
+console.log(c1);
+console.log(c2);
+console.log(c3);
+console.log(c4);
+
+
